3b0141870f42845c72360a6bf128765eabdcdcd2
Nathan Lepore <nathaniel.lepore@students.olin.edu>
2017-03-11 17:47:13 -0500

    Turning in my pickling toolbox

--- counter.py
+++ counter.py
@@ -1,8 +1,8 @@
 """ A program that stores and updates a counter using a Python pickle file"""
 
-from os.path import exists
+import os.path
+import pickle
 import sys
-from pickle import dump, load
 
 
 def update_counter(file_name, reset=False):
@@ -30,7 +30,31 @@ def update_counter(file_name, reset=False):
     >>> update_counter('blah2.txt')
     2
     """
-    pass
+    counter = 0
+
+    if os.path.exists(file_name) is True and reset is True:
+        filehandle = open(file_name, "wb")
+        counter = 1
+        pickle.dump(counter, filehandle)
+        filehandle.close()
+        return counter
+    elif os.path.exists(file_name) is True and reset is False:
+        file = open(file_name, "rb")
+        file.seek(0, 0)
+        counter = pickle.load(file)
+        counter += 1
+        file.close()
+        filehandle = open(file_name, "wb")
+        pickle.dump(counter, filehandle)
+        filehandle.close()
+        return counter
+    elif os.path.exists(file_name) is False:
+        filehandle = open(file_name, "wb")
+        counter = 1
+        pickle.dump(counter, filehandle)
+        filehandle.close()
+        return counter
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
